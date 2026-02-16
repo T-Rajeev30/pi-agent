6c2848fe9c871092a3b2597698de96b7dc28befe
Rajeev Tiwari <113722532+T-Rajeev30@users.noreply.github.com>
2026-02-16 19:36:10 +0530

    Update converter.js

--- camera/converter.js
+++ camera/converter.js
@@ -1,22 +1,19 @@
 const { spawn } = require("child_process");
 
-function convert(input, output) {
+module.exports = (input, output) => {
   return new Promise((resolve, reject) => {
     const ff = spawn("ffmpeg", [
       "-y",
       "-fflags", "+genpts",
-      "-r", "60",
       "-i", input,
-      "-c:v", "copy",
+      "-c", "copy",
       "-movflags", "+faststart",
       output
     ]);
 
-    ff.on("close", (code) => {
+    ff.on("close", code => {
       if (code === 0) resolve();
       else reject(new Error("ffmpeg failed"));
     });
   });
-}
-
-module.exports = convert;
+};
